ad0045648312887edb175d27b56e9f4a494d4b30
ShreyaPMohapatra <shreyapmohapatra@gmail.com>
2025-08-14 03:28:48 +0530

    Fix: Set correct baseURL in axios config

--- client/src/apis/RestaurantFinder.js
+++ client/src/apis/RestaurantFinder.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://restaurant-finder-0xet.onrender.com/",
+  // Manually set the full URL here
+  baseURL: "https://restaurant-finder-0xet.onrender.com/api/v1/restaurants",
 });
