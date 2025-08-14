fee26923b2f06dba2598acdb49c56bbc801c6966
ShreyaPMohapatra <shreyapmohapatra@gmail.com>
2025-08-15 01:36:22 +0530

    Move build files to server and fix API URL

--- client/src/apis/RestaurantFinder.js
+++ client/src/apis/RestaurantFinder.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  // The baseURL should be the root of your API, which is your live Render URL
   baseURL: "https://restaurant-finder-ao74.onrender.com/api/v1",
 });
