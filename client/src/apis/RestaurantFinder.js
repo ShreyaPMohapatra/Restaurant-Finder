79cb67c74d61ef660dbc45bc8a41317b8e888bca
ShreyaPMohapatra <shreyapmohapatra@gmail.com>
2025-08-15 00:46:18 +0530

    Update frontend API URL and redeploy

--- client/src/apis/RestaurantFinder.js
+++ client/src/apis/RestaurantFinder.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  // The baseURL should be the root of the API
-  baseURL: "https://restaurant-finder-0xet.onrender.com/api/v1",
+  // The baseURL should be the root of your API, which is your live Render URL
+  baseURL: "https://restaurant-finder-ao74.onrender.com/api/v1",
 });
